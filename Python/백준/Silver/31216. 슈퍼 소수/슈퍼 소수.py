f22f3dbca5de44da89e296becbae929c7108000b
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2026-04-01 00:27:18 +0900

    [Silver V] Title: 슈퍼 소수, Time: 72 ms, Memory: 35920 KB -BaekjoonHub

--- Python/백준/Silver/31216. 슈퍼 소수/슈퍼 소수.py
+++ Python/백준/Silver/31216. 슈퍼 소수/슈퍼 소수.py
@@ -1,17 +1,22 @@
-import math
 import sys
 input = sys.stdin.readline
-
-def get_primes(n):
+def get_prime_primes(n):
     sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False
+
     for p in range(2, int(n**0.5) + 1):
         if sieve[p]:
             for i in range(p * p, n + 1, p):
                 sieve[i] = False
-    return [p for p in range(2, n + 1) if sieve[p]]
-
-superPrime = get_primes(318137)
+    primes = [p for p in range(2, n + 1) if sieve[p]]
+    prime_primes = [
+        primes[i]
+        for i in range(len(primes))
+        if sieve[i + 1]
+    ]
+    return prime_primes
 T = int(input())
-for i in range(T):
+superPrime = get_prime_primes(318137)
+for _ in range(T):
     n = int(input())
-    print(superPrime[superPrime[n-1]-1])
+    print(superPrime[n-1])
