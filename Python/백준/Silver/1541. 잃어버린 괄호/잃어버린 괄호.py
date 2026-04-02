19080262d785749cbf87eafa9057591b35390b67
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2026-04-02 19:50:17 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ Python/백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,5 +1,4 @@
-import sys
-add_part_list = sys.stdin.readline().strip().split('-')
+add_part_list = input().split('-')
 result = sum(map(int, add_part_list[0].split('+')))
 for add_part in add_part_list[1:]:
     result -= sum(map(int, add_part.split('+')))
