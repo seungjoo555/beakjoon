0f04ce486f598a08e68df7ce27d01f04e8591f26
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2026-04-02 13:34:08 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ Python/백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,10 +1,11 @@
 N = int(input())
-bag = []
-for i in range(N//5+1):
+bag = 0
+for i in range(N//5, -1, -1):
     for j in range(N//3+1):
         if N == ((5*i) + (3*j)):
-            bag.append(i+j)
-if len(bag):
-    print(min(bag))
+            bag = i+j
+    if bag:
+        print(bag)
+        break
 else:
     print(-1)
