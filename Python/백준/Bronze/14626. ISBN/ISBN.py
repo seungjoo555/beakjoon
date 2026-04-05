f90c586edd579ed3126c1e3cc4473723450f6e43
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2026-04-06 00:47:48 +0900

    [Bronze I] Title: ISBN, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/14626. ISBN/ISBN.py
+++ Python/백준/Bronze/14626. ISBN/ISBN.py
@@ -1,27 +1,8 @@
-ISBN = (input())
+ISBN = (' '.join(input()).split())
 check = ISBN.index('*')
-a = ' '.join(ISBN[0:12:2]).split()
-b = ' '.join(ISBN[1::2]).split()
-if check == 12:
-    a = list(map(int, a))
-    b = list(map(int, b))
-    for i in range(10):
-        if (sum(a) + (sum(b)*3) + i) % 10 == 0:
-            print(i)
-            break
-elif check%2 == 0:
-    a.remove('*')
-    a = list(map(int, a))
-    b = list(map(int, b))
-    for i in range(10):
-        if (sum(a)+ i + (sum(b)*3) + int(ISBN[-1])) % 10 == 0:
-            print(i)
-            break
-else:
-    b.remove('*')
-    a = list(map(int, a))
-    b = list(map(int, b))
-    for i in range(10):
-        if (sum(a) + ((sum(b)+ i)*3) + int(ISBN[-1])) % 10 == 0:
-            print(i)
-            break
+for i in range(10):
+    ISBN[check] = i
+    ISBN = list(map(int, ISBN))
+    if (sum(ISBN[0::2]) + (sum(ISBN[1::2]) * 3)) % 10 == 0:
+        print(i)
+        break
