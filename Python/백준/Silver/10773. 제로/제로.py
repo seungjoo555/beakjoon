fa3e412d107559d26bb3c0efea48dbe2967c49af
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2026-04-06 10:17:07 +0900

    [Silver IV] Title: 제로, Time: 72 ms, Memory: 36560 KB -BaekjoonHub

--- Python/백준/Silver/10773. 제로/제로.py
+++ Python/백준/Silver/10773. 제로/제로.py
@@ -2,10 +2,12 @@ import sys
 input = map(int, sys.stdin.read().split())
 K = next(input)
 nist = []
+sum = 0
 for _ in range(K):
     n = next(input)
     if n != 0:
         nist.append(n)
+        sum += n
     else:
-        nist.pop()
-print(sum(nist))
+        sum -= nist.pop()
+print(sum)
