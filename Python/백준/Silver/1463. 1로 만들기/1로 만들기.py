6b2b238f6c8dda98acacb7af6e4d3997a189ff9f
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2026-04-07 20:49:25 +0900

    [Silver III] Title: 1로 만들기, Time: 460 ms, Memory: 40224 KB -BaekjoonHub

--- Python/백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ Python/백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -2,11 +2,10 @@ N = int(input())
 
 dpList = [0] * (N+1)
 for i in range(2,N+1):
-    a = dpList[i-1]
-    if i % 3 == 0:
-        a = min(dpList[i//3], a)
+    dpList[i] = dpList[i-1]+1
     if i % 2 == 0:
-        a = min(dpList[i//2], a)
-    dpList[i] = a+1
+        dpList[i] = min(dpList[i//2]+1, dpList[i])
+    if i % 3 == 0:
+        dpList[i] = min(dpList[i//3]+1, dpList[i])
 
 print(dpList[N])
