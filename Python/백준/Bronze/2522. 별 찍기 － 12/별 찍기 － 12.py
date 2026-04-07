f163ab82d3aae3c2db04bcb4fe074d890355ae04
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2026-04-08 01:02:24 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
+++ Python/백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
@@ -1,8 +1,6 @@
-def s():
-    import sys
-    n = int(sys.stdin.readline())
-    for i in range(n):
-        print(' ' * ((n-1)-i)+ '*' * (1+i))
-    for i in range(n-1):
-        print(' ' * (1+i)+ '*' * ((n-1)-i))
-s()
+import sys
+n = int(sys.stdin.readline())
+for i in range(n):
+    print(' ' * ((n-1)-i)+ '*' * (1+i))
+for i in range(n-1):
+    print(' ' * (1+i)+ '*' * ((n-1)-i))
