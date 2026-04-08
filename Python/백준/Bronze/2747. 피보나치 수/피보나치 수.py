409fbd0e1674aa4d78292f9fc33534b673bfb0b8
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2026-04-08 11:18:02 +0900

    [Bronze II] Title: 피보나치 수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2747. 피보나치 수/피보나치 수.py
+++ Python/백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -1,5 +1,8 @@
+def pibo(num):
+    if fibo[num] == -1:
+        fibo[num] = pibo(num-1) + pibo(num-2)
+    return fibo[num]
+
 n = int(input())
-pibo = [0,1]+[0]*(n-1)
-for i in range(2, len(pibo)):
-    pibo[i] = pibo[i-1] + pibo[i-2]
-print(pibo[n])
+fibo = [0,1]+[-1]*(n-1)
+print(pibo(n))
