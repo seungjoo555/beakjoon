1ce6144cc9bcdd4f7d7b3ffb314845c0a585a9c0
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2026-04-08 18:56:59 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ Python/백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,9 +1,11 @@
-T = int(input())
-for _ in range(T):
-    k = int(input())
-    n = int(input())
-    ho = [i for i in range(0,n+1)]
-    for _ in range(k):
-        for i in range(n):
-            ho[i+1] = ho[i]+ho[i+1]
-    print(ho[n])
+def zz():
+    T = int(input())
+    for _ in range(T):
+        k = int(input())
+        n = int(input())
+        ho = [i for i in range(0,n+1)]
+        for _ in range(k):
+            for i in range(n):
+                ho[i+1] = ho[i]+ho[i+1]
+        print(ho[n])
+zz()
