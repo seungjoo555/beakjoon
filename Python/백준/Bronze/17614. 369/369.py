cf8dafb414b114ef9ae36d2c88b40a80d47281fc
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2026-04-15 01:04:36 +0900

    [Bronze III] Title: 369, Time: 648 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- Python/백준/Bronze/17614. 369/369.py
+++ Python/백준/Bronze/17614. 369/369.py
@@ -1,7 +1,5 @@
 N = int(input())
 s = 0
 for i in range(1, N+1):
-    s += str(i).count('3')
-    s += str(i).count('6')
-    s += str(i).count('9')
+    s += str(i).count('3')+str(i).count('6')+str(i).count('9')
 print(s)
