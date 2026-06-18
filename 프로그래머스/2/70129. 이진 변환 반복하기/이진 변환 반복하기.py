ecffc6a5efea33a5c8227ad7f337f6a07ad10d09
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2026-06-18 13:03:16 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 2.48 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,8 @@
+def solution(s):
+    answer = [0, 0]
+    while s != '1':
+        answer[0] += 1
+        answer[1] += (s.count("0"))
+        s = s.replace("0", "")
+        s = str(format(len(s), 'b'))
+    return answer
