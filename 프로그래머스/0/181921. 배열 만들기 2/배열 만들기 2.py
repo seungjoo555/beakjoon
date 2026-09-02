45de8089bc482cffc833a303ac40bd3fdb3cd2c5
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2026-09-02 11:04:45 +0900

    [level 0] Title: 배열 만들기 2, Time: 275.43 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,6 @@
+def solution(l, r):
+    answer = []
+    for i in range(l, r+1):
+        if set(str(i)).issubset({'0', '5'}): answer.append(i)
+    if not answer: answer.append(-1)
+    return answer
